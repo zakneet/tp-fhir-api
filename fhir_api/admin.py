3276a02b39a3157d2327c21d909c264812c7a463
Zakaria Fekih Ahmed <zakariafekihahmed@gmail.com>
2026-03-05 13:26:19 +0100

    Add Patient/Observation models and admin

--- fhir_api/admin.py
+++ fhir_api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Patient, Observation
 
-# Register your models here.
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ("id", "identifier", "family_name", "given_name", "gender", "birth_date", "created_at")
+    search_fields = ("family_name", "given_name", "identifier")
+    list_filter = ("gender",)
+
+@admin.register(Observation)
+class ObservationAdmin(admin.ModelAdmin):
+    list_display = ("id", "patient", "observation_type", "value", "unit", "effective_date", "created_at")
+    list_filter = ("observation_type", "effective_date")
+    search_fields = ("patient__family_name", "patient__given_name")
